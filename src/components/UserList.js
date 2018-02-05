9b333b80e245f01c2afa36526a4ab9e5e12c3aab
Wei Wei <weiway@outlook.com>
2018-02-04 21:07:38 -0700

    Optimize user list card.group layout for mobile

--- src/components/UserList.js
+++ src/components/UserList.js
@@ -5,7 +5,7 @@ import UserListItem from './UserListItem';
 
 const UserList = ({ users }) => {
   return (
-    <Card.Group itemsPerRow="2">
+    <Card.Group itemsPerRow="2" doubling={true} stackable={true}>
       {users.map((user, index) => (<UserListItem key={index} {...user} />))}
     </Card.Group>
   );
